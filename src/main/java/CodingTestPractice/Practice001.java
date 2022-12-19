8292e6f5bca523f262de062fc5edb2499d09d617
UngHeung <t01047377477@gmail.com>
2022-12-19 14:26:41 +0900

    feat : 입력된 숫자를 문자열로 변환하는 기능 추가

---
+++ src/main/java/CodingTestPractice/Practice001.java
@@ -0,0 +1,31 @@
+package CodingTestPractice;
+
+public class Practice001 {
+	/**
+	 * 자릿수 더하기
+	 * 
+	 * 문제
+	 * 정수 n이 매개변수로 주어질 때
+	 * n의 각 자리 숫자의 합을 return하도록
+	 * solution 함수를 완성해주세요
+	 * 
+	 * 제한사항
+	 * 0 <= n <= 1,000,000
+	 * 
+	 * 입출력 예 (n -> result)
+	 * 1234 -> 10
+	 * 930211 -> 16
+	 */
+}
+
+class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		return answer;
+	}
+	
+	private String numberToString(int n) {
+		String result = Integer.toString(n);
+		return result;
+	}
+}
