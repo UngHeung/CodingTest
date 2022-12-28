65e60022a38c04051152c2bee35a120da87cb069
UngHeung <t01047377477@gmail.com>
2022-12-28 09:11:29 +0900

    feat : gredient element를 문자열로 저장하는 기능 추가

---
+++ src/main/java/CodingTestPractice/Practice005.java
@@ -0,0 +1,16 @@
+package CodingTestPractice;
+
+public class Practice005 {
+}
+
+class Solution {
+	public int solution(int[] ingredient) {
+		int answer = 0;
+		
+		String str = "";
+		for (int element : ingredient) {
+			str += element;
+		}
+		return answer;
+	}
+}
