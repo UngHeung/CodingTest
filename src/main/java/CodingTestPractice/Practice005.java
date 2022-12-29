ea64f2a4b5e770f0b619088e2ffe344cd2211a76
UngHeung <t01047377477@gmail.com>
2022-12-29 10:12:46 +0900

    refactor : 시간 초과 및 실패로 코드 수정

--- src/main/java/CodingTestPractice/Practice005.java
+++ src/main/java/CodingTestPractice/Practice005.java
@@ -1,30 +1,16 @@
 package CodingTestPractice;
 
 public class Practice005 {
+	
 }
 
 class Solution {
 	public int solution(int[] ingredient) {
 		int answer = 0;
 		
-		String str = "";
 		for (int element : ingredient) {
-			str += element;
-		}
-		
-		int ingredientCount = ingredient.length;
-		int length = 0;
-		while (true) {
-			length = str.length();
-			str = str.replace("1231", "");
 			
-			if (length == str.length()) {
-				break;
-			}
 		}
-		
-		answer = (ingredientCount - length) / 4;
-		
 		return answer;
 	}
 }
