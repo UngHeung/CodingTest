b6f4e8e60ca85b7ec3db2ff1722cf8dd5655bc3a
UngHeung <t01047377477@gmail.com>
2023-01-01 04:53:49 +0900

    feat : 대문자를 소문자로 변경하는 기능 추가

---
+++ src/main/java/CodingTestPractice/Practice007.java
@@ -0,0 +1,31 @@
+package CodingTestPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice007 {
+	/**
+	 * 신규 아이디 추천
+	 * 
+	 * 문자열 new_id가 매개변수로 넘어올 때
+	 * 7개의 규칙을 기준으로 유효성 검사 및 규칙에 맞는 id를 반환한다.
+	 * 
+	 * 1. 대문자 -> 소문자
+	 * 2. 소문자, 숫자, "-", "_", "."를 제외한 모든 문자 제거
+	 * 3. 마침표가 2번 이상 연속될 경우 하나로 치환
+	 * 4. 마침표가 처음 혹은 끝에 위치할 경우 제거
+	 * 5. 빈 문자열인 경우 "a"를 대입
+	 * 6. 길이가 16자 이상인 경우 첫 15자 외 제거
+	 * 7. 길이가 2자 이하인 경우 마지막 문자를 길이가 3이 될 때까지 반복하여 끝에 추가
+	 */
+}
+
+class Solution {
+	private List<Character> recommendId = new ArrayList<>();
+	
+	public String solution(String new_id) {
+		String answer = "";
+		new_id = new_id.toLowerCase();
+		return answer;
+	}
+}
