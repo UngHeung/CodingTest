4e02b64e3e24c496ef1915bdcb6c7bf9b6b4ec3a
UngHeung <t01047377477@gmail.com>
2023-01-04 19:22:18 +0900

    test : 테스트 코드 작성 및 테스트 케이스 추가

---
+++ src/test/java/CodingTestPractice/Practice008Test.java
@@ -0,0 +1,53 @@
+package CodingTestPractice;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Practice008Test {
+	Solution sol = new Solution();
+	
+	private int[][] boardCase01 = {
+			{0, 1, 0, 0, 1}, 
+			{0, 3, 0, 0, 3}, 
+			{2, 4, 0, 0, 2}, 
+			{4, 1, 0, 2, 4}, 
+			{1, 5, 1, 1, 5}};
+	
+	private int[][] boardCase02 = {
+			{0, 0, 0, 0, 0}, 
+			{0, 0, 1, 0, 3}, 
+			{0, 2, 5, 0, 1}, 
+			{4, 2, 4, 4, 2}, 
+			{3, 5, 1, 3, 1}};
+	
+	private int[][] boardCase03 = {
+			{0, 0, 0, 0, 0}, 
+			{0, 0, 1, 0, 3}, 
+			{0, 2, 5, 0, 1}, 
+			{4, 2, 4, 4, 2}, 
+			{3, 5, 1, 3, 1}};
+	
+	private int[] movesCase01 = {2, 5, 2, 5, 1, 1, 2, 3};
+	private int[] movesCase02 = {1, 5, 3, 5, 1, 2, 1, 4};
+	private int[] movesCase03 = {1, 5, 3, 5, 1, 2, 1, 4};
+	
+	private int expectedResultCase01 = 6;
+	private int expectedResultCase02 = 4;
+	private int expectedResultCase03 = 4;
+	
+	@Test
+	public void test01() {
+		assertEquals(sol.solution(boardCase01, movesCase01), expectedResultCase01);
+	}
+	
+	@Test
+	public void test02() {
+		assertEquals(sol.solution(boardCase02, movesCase02), expectedResultCase02);
+	}
+	
+	@Test
+	public void test03() {
+		assertEquals(sol.solution(boardCase03, movesCase03), expectedResultCase03);
+	}
+}
