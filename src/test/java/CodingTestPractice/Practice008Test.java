4d1a113a031035a3428401e49fe1cf2cfd6b452f
UngHeung <t01047377477@gmail.com>
2023-01-04 22:30:09 +0900

    test : 테스트 케이스 추가

--- src/test/java/CodingTestPractice/Practice008Test.java
+++ src/test/java/CodingTestPractice/Practice008Test.java
@@ -23,18 +23,18 @@ public class Practice008Test {
 	
 	private int[][] boardCase03 = {
 			{0, 0, 0, 0, 0}, 
-			{0, 0, 1, 0, 3}, 
-			{0, 2, 5, 0, 1}, 
-			{4, 2, 4, 4, 2}, 
-			{3, 5, 1, 3, 1}};
+			{0, 2, 1, 0, 0}, 
+			{0, 5, 2, 0, 2}, 
+			{0, 4, 1, 3, 2}, 
+			{1, 3, 5, 2, 3}};
 	
 	private int[] movesCase01 = {2, 5, 2, 5, 1, 1, 2, 3};
 	private int[] movesCase02 = {1, 5, 3, 5, 1, 2, 1, 4};
-	private int[] movesCase03 = {1, 5, 3, 5, 1, 2, 1, 4};
+	private int[] movesCase03 = {4, 3, 1, 2, 3, 3, 5, 4};
 	
 	private int expectedResultCase01 = 6;
 	private int expectedResultCase02 = 4;
-	private int expectedResultCase03 = 4;
+	private int expectedResultCase03 = 6;
 	
 	@Test
 	public void test01() {
