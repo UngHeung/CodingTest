84177e5e3469f24a83c7bdb519d09d1656b23464
UngHeung <t01047377477@gmail.com>
2023-01-07 00:34:31 +0900

    feat : 랭크를 확인할 수 있는 배열 추가, 최고, 최저등수를 반환하는 기능

--- src/main/java/CodingTestPractice/Practice010.java
+++ src/main/java/CodingTestPractice/Practice010.java
@@ -17,10 +17,12 @@ class Solution {
 			}
 			checkNumber(lotto);
 		}
+		answer = checkRank();
 		return answer;
 	}
 	
 	private int[] lottoNumbers = new int[46];
+	private int[] rank = {6, 6, 5, 4, 3, 2, 1};
 	private int countDeleteNumber = 0;
 	private int countLotto = 0;
 	
@@ -28,5 +30,13 @@ class Solution {
 		if (lottoNumbers[lotto] == 1) {
 			countLotto++;
 		}
+	}
+	
+	private int[] checkRank() {
+		int[] result = new int[2];
+		int countMaximumLotto = countLotto + countDeleteNumber;
+		result[0] = rank[countMaximumLotto];
+		result[1] = rank[countLotto];
+		return result;
 	}
 }
