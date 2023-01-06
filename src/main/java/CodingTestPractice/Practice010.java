af5e1038c86970cf11d0e0174eb5ac8c234ec7ec
UngHeung <t01047377477@gmail.com>
2023-01-07 00:32:58 +0900

    feat : 지워진 번호 개수, 당첨 번호 개수를 셀 변수 추가, 지워진 번호 개수 카운트 기능 추가

--- src/main/java/CodingTestPractice/Practice010.java
+++ src/main/java/CodingTestPractice/Practice010.java
@@ -4,14 +4,22 @@ public class Practice010 {
 	
 }
 
-class Solution0A {
+class Solution {
 	public int[] solution(int[] lottos, int[] win_nums) {
 		int[] answer = {};
 		for (int number : win_nums) {
 			lottoNumbers[number]++;
 		}
+		
+		for (int lotto : lottos) {
+			if (lotto == 0) {
+				countDeleteNumber++;
+			}
+		}
 		return answer;
 	}
 	
 	private int[] lottoNumbers = new int[46];
+	private int countDeleteNumber = 0;
+	private int countLotto = 0;
 }
